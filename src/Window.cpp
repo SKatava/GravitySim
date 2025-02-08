b82f8ea4d3e2c590d7a0c6514f40ace6283de52c
SKatava <katava.sven@gmail.com>
2025-02-08 17:33:08 +0100

    Changed the start-up scenario

--- src/Window.cpp
+++ src/Window.cpp
@@ -85,9 +85,12 @@ void Window::Draw(){
 	double lastTime = glfwGetTime();
 	double currentTime;
 
-	OBJMG::AddObject(1, Vector2f(300.f, 500.f), Vector3f(1.f, 0.f, 0.f));	
-	OBJMG::AddObject(1000000000, Vector2f(500.f, 500.f), Vector3f(0.f, 0.f, 1.f));
-	OBJMG::objects[0].SetInitialVelocity(Vector2f(0.f, std::sqrt(0.000667430 * 1000000000 / 200)));
+	OBJMG::AddObject(100000, Vector2f(800.f, 500.f), Vector3f(1.f, 0.f, 0.f));	
+	OBJMG::AddObject(100000, Vector2f(200.f, 500.f), Vector3f(0.f, 1.f, 0.f));
+	OBJMG::AddObject(100000, Vector2f(500.f, 500.f), Vector3f(0.f, 0.f, 1.f));
+	OBJMG::objects[0].SetInitialVelocity(Vector2f(0.f, std::sqrt(0.000667430 * 100000 / 450)));
+	OBJMG::objects[1].SetInitialVelocity(Vector2f(0.f, -std::sqrt(0.000667430 * 100000 / 450)));
+	OBJMG::objects[2].SetInitialVelocity(Vector2f(0.f, 0.f));
 	STOP_SIM = 1;
     while (!glfwWindowShouldClose(window)){
 		glClearColor(0.0f, 0.0f, 0.f, 1.f);
