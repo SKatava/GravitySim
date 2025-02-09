5822a2f34a25eaf6339a55c82f13293e8f34daf3
SKatava <katava.sven@gmail.com>
2025-02-09 18:22:29 +0100

    Optimized the collision detection, algorithm cost reduced by two times

--- src/CollisionSolution.cpp
+++ src/CollisionSolution.cpp
@@ -4,30 +4,27 @@
 void CSL::Check(){
     int size = OBJMG::objects.size();
     for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            if(i != j){
-                float distanceX = OBJMG::objects[i].GetPos().x - OBJMG::objects[j].GetPos().x;
-                float distanceY = OBJMG::objects[i].GetPos().y - OBJMG::objects[j].GetPos().y; 
-                if(std::abs(distanceX) < 25 && std::abs(distanceY) < 25){
-                    float massA = OBJMG::objects[i].GetMass();
-                    float massB = OBJMG::objects[j].GetMass();
-                    Vector2f velA = OBJMG::objects[i].GetVelocity();
-                    Vector2f velB = OBJMG::objects[j].GetVelocity();
-                    if(OBJMG::objects[i].GetMass() > OBJMG::objects[j].GetMass()){
-                        Solution(massA, massB, velA, velB, OBJMG::objects[i].GetPos());
-
-                    }
-                    else if(OBJMG::objects[i].GetMass() < OBJMG::objects[j].GetMass()){
-                        Solution(massA, massB, velA, velB, OBJMG::objects[j].GetPos());
-                    }
-                    else{
-                        Vector2f pos = Vector2f( (OBJMG::objects[i].GetPos().x + OBJMG::objects[j].GetPos().x) / 2, (OBJMG::objects[i].GetPos().y + OBJMG::objects[j].GetPos().y) / 2);
-                        Solution(massA, massB, velA, velB, pos);
-                    }
-                    OBJMG::Delete(i);
-                    OBJMG::Delete(j-1);
-                    size--;
+        for(int j = i+1; j < size; j++){
+        float distanceX = OBJMG::objects[i].GetPos().x - OBJMG::objects[j].GetPos().x;
+        float distanceY = OBJMG::objects[i].GetPos().y - OBJMG::objects[j].GetPos().y; 
+            if(std::abs(distanceX) < 25 && std::abs(distanceY) < 25){
+                float massA = OBJMG::objects[i].GetMass();
+                float massB = OBJMG::objects[j].GetMass();
+                Vector2f velA = OBJMG::objects[i].GetVelocity();
+                Vector2f velB = OBJMG::objects[j].GetVelocity();
+                if(OBJMG::objects[i].GetMass() > OBJMG::objects[j].GetMass()){
+                    Solution(massA, massB, velA, velB, OBJMG::objects[i].GetPos());
                 }
+                else if(OBJMG::objects[i].GetMass() < OBJMG::objects[j].GetMass()){
+                    Solution(massA, massB, velA, velB, OBJMG::objects[j].GetPos());
+                }
+                else{
+                    Vector2f pos = Vector2f( (OBJMG::objects[i].GetPos().x + OBJMG::objects[j].GetPos().x) / 2, (OBJMG::objects[i].GetPos().y + OBJMG::objects[j].GetPos().y) / 2);
+                    Solution(massA, massB, velA, velB, pos);
+                }
+                OBJMG::Delete(i);
+                OBJMG::Delete(j-1);
+                size--;
             }
         }
     }
